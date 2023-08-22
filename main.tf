d8c1c23da400c5a7b96587e686d372cc5c7617ef
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 20:33:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ resource "aws_route_table" "private" {
 
 
 resource "aws_route_table_association" "private" {
-  count          = 4
   subnet_id      = aws_subnet.private_subnets.id
   route_table_id = aws_route_table.private.id
 }
