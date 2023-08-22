c6b97332f674501718439027d56b102e2e5ef4c0
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 12:00:25 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -1,1 +1,11 @@
+data "aws_nat_gateway" "nat" {
+  id = "nat-0d688bbff8a47b274"
+}
 
+data "aws_vpc" "vpc" {
+  id = "vpc-00bf0d10a6a41600c"
+}
+
+data "aws_iam_role" "lambda" {
+  name = "DevOps-Candidate-Lambda-Role"
+}
