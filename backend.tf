9b2f8a2b052275520354485a421fa487f886bc4a
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 13:05:38 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,3 +1,12 @@
-S3 Bucket: "3.devops.candidate.exam"
-Region: "ap-south-1"
-Key: "s3/yogeshbhagwat"
+#S3 Bucket: "3.devops.candidate.exam"
+#Region: "ap-south-1"
+#Key: "s3/yogeshbhagwat"
+
+terraform {
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    key    = "yogeshbhagwat"
+    region = "ap-south-1"
+  }
+}
+
