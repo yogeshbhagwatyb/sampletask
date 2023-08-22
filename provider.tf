d5a7d7abd7517a9db2bfc2ffa8b72a1a63ea9109
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 20:50:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,4 +2,10 @@ provider "aws" {
   region  = "ap-south-1" # Don't change the region
 }
 
-# Add your S3 backend configuration here
+terraform {
+  backend "s3" {
+    bucket = "3.devops.candidate.exam"
+    key    = "yogeshbhagwat"
+    region = "ap-south-1"
+  }
+}
