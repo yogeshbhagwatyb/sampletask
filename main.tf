abd4be3aef67e1045e2b043c561bcd43ae2b9b0f
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 14:34:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,11 +67,11 @@ resource "aws_security_group" "lambda_sg" {
 
 
 
-#resource "aws_security_group_rule" "lambda_ingress" {
-#  type        = "ingress"
-#  from_port   = 0
-#  to_port     = 65535
-#  protocol    = "tcp"
-#  cidr_blocks = ["0.0.0.0/0"]
-#  security_group_id = aws_security_group.lambda_sg.id
-#}
+resource "aws_security_group_rule" "lambda_ingress" {
+  type        = "ingress"
+  from_port   = 0
+  to_port     = 65535
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lambda_sg.id
+}
