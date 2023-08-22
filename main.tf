bc1f028455b3fea7a1f5bdd2e0a90d7462b2b982
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 15:57:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,12 +35,12 @@ resource "aws_security_group" "lambda_sg" {
 }
 
 resource "aws_lambda_function" "example" {
-   function_name    = "lambda_function.py"
+   function_name    = "lambda_function"
    role             = data.aws_iam_role.lambda.arn
    handler          = "lambda_function.py"
-   #runtime          = "python 3.10"
-   filename         = "path/to/lambda.zip"
-  source_code_hash = filebase64sha256("path/to/lambda.zip")
+  # runtime          = ""
+   #filename         = "path/to/lambda.zip"
+ 
 
   vpc_config {
      subnet_ids         = aws_subnet.private_subnets[*].id
