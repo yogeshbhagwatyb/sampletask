d6924f6b8ecc1410519205be9fcfcd6032acb93f
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 21:13:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_lambda_function" "yogesh_function" {
  
 
   vpc_config {
-     subnet_ids          = aws_subnet.private_subnets.id
-     security_group_ids = aws_security_group.lambda_sg.id
+     subnet_ids          = aws_subnet.private_subnets.ids
+     security_group_ids = aws_security_group.lambda_sg.ids
    }
  }
