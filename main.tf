ac640d0de27c1b4a274e3fb826dece6fdefc2772
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 15:47:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_lambda_function" "example" {
    function_name    = "lambda_function.py"
    role             = data.aws_iam_role.lambda.arn
    handler          = "lambda_function.py"
-   runtime          = "python 3.10"
+   #runtime          = "python 3.10"
    filename         = "path/to/lambda.zip"
   source_code_hash = filebase64sha256("path/to/lambda.zip")
 
