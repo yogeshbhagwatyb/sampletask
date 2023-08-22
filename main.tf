d1900133a83c10d79520f065be3d66940e6802c4
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 13:55:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,8 @@ resource "aws_subnet" "private_subnets" {
   #count = 4
 
   #cidr_block = "10.0.${count.index + 1}.0/24"
- cidr_block = ["10.0.20.0/24", "10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+ #cidr_block = ["10.0.20.0/24", "10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+cidr_block = ["10.0.20.0/24"]
 
   vpc_id     = data.aws_vpc.vpc.id
 }
