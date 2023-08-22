750e5897e22f14e29d1b7c2ecec212dc70a2774c
yogeshbhagwatyb <139680401+yogeshbhagwatyb@users.noreply.github.com>
2023-08-22 14:37:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,28 +50,28 @@ resource "aws_security_group" "lambda_sg" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
-#resource "aws_lambda_function" "example" {
-#   function_name    = "example-lambda"
-#   role             = data.aws_iam_role.lambda.arn
-#   handler          = "index.handler"
-#   runtime          = "nodejs14.x"
-#   filename         = "path/to/lambda.zip"
-#   source_code_hash = filebase64sha256("path/to/lambda.zip")
+resource "aws_lambda_function" "example" {
+   function_name    = "example-lambda"
+   role             = data.aws_iam_role.lambda.arn
+   handler          = "index.handler"
+   runtime          = "nodejs14.x"
+   filename         = "path/to/lambda.zip"
+   source_code_hash = filebase64sha256("path/to/lambda.zip")
 
-#   vpc_config {
-#     subnet_ids         = aws_subnet.private_subnets[*].id
-#     security_group_ids = [aws_security_group.lambda_sg.id]
-#   }
-# }
-
-
+   vpc_config {
+     subnet_ids         = aws_subnet.private_subnets[*].id
+     security_group_ids = [aws_security_group.lambda_sg.id]
+   }
+ }
 
 
-resource "aws_security_group_rule" "lambda_ingress" {
-  type        = "ingress"
-  from_port   = 0
-  to_port     = 65535
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.lambda_sg.id
-}
+
+
+#resource "aws_security_group_rule" "lambda_ingress" {
+#  type        = "ingress"
+#  from_port   = 0
+#  to_port     = 65535
+#  protocol    = "tcp"
+#  cidr_blocks = ["0.0.0.0/0"]
+#  security_group_id = aws_security_group.lambda_sg.id
+#}
